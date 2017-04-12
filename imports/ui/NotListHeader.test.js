fdc2b773caaf056c54d9c50b79fe5aa685b18021
Xavok <xavoking@gmail.com>
2017-04-12 01:37:05 -0700

    Select newly created note.Sort notes by updatedAt

--- imports/ui/NotListHeader.test.js
+++ imports/ui/NotListHeader.test.js
@@ -6,14 +6,34 @@ import React from 'react';
 import expect from 'expect';
 import {mount} from 'enzyme';
 import {NoteListHeader} from './NoteListHeader';
+import {notes} from '../fixtures/fixtures';
 
 if (Meteor.isClient) {
     describe('NoteListHeader', function () {
+        let meteorCall;
+        let Session;
+
+        beforeEach(function () {
+            meteorCall = expect.createSpy(),
+                Session = {
+                    set: expect.createSpy()
+                }
+        });
+
         it('should call meteorCall on click', function () {
-            const spy = expect.createSpy();
-            const wrapper = mount(<NoteListHeader meteorCall={spy}/>);
+            const wrapper = mount(<NoteListHeader meteorCall={meteorCall} Session={Session}/>);
             wrapper.find('button').simulate('click');
-            expect(spy).toHaveBeenCalledWith('notes.insert');
+            meteorCall.calls[0].arguments[1](undefined, notes[0]._id);
+            expect(Session.set).toHaveBeenCalledWith('selectedNoteId',notes[0]._id);
+            expect(meteorCall.calls[0].arguments[0]).toBe('notes.insert');
+        });
+
+        it('should not set Session for failed insert',function() {
+            const wrapper = mount(<NoteListHeader meteorCall={meteorCall} Session={Session}/>);
+            wrapper.find('button').simulate('click');
+            meteorCall.calls[0].arguments[1]('aad', undefined);
+            expect(meteorCall.calls[0].arguments[0]).toBe('notes.insert');
+            expect(Session.set).toNotHaveBeenCalled();
         });
 
     });
--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -4,21 +4,28 @@
 import {Meteor} from 'meteor/meteor';
 import React from 'react';
 import {createContainer} from 'meteor/react-meteor-data';
+import {Session} from 'meteor/session';
 
 export const NoteListHeader = (props) => {
     return (
         <div>
-            <button onClick={() => props.meteorCall('notes.insert')}>Create Note</button>
+            <button onClick={() => props.meteorCall('notes.insert', (err, res) => {
+                if(res) {
+                    props.Session.set('selectedNoteId', res);
+                }
+            })}>Create Note</button>
         </div>
     );
 };
 
 NoteListHeader.propTypes = {
-    meteorCall: React.PropTypes.func.isRequired
+    meteorCall: React.PropTypes.func.isRequired,
+    Session:React.PropTypes.object.isRequired,
 };
 
 export default createContainer(() => {
     return {
-        meteorCall: Meteor.call
+        meteorCall: Meteor.call,
+        Session
     }
 }, NoteListHeader);
