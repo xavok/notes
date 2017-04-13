27e98bcd5ae9e4ec569db2afd18ce5d1a6298685
Xavok <xavoking@gmail.com>
2017-04-13 00:40:36 -0700

    Setup mobile navigation

--- client/main.js
+++ client/main.js
@@ -1,25 +1,30 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 import ReactDOM from 'react-dom';
-import { Tracker } from 'meteor/tracker';
-import { Session } from 'meteor/session';
+import {Tracker} from 'meteor/tracker';
+import {Session} from 'meteor/session';
 import {browserHistory} from 'react-router';
-import { routes, onAuthChange } from '../imports/routes/routes';
+import {routes, onAuthChange} from '../imports/routes/routes';
 import '../imports/startup/simple-schema-configuration.js';
 
 Tracker.autorun(() => {
-  const isAuthenticated = !!Meteor.userId();
-  const currentPagePrivacy = Session.get('currentPagePrivacy');
-  onAuthChange(isAuthenticated,currentPagePrivacy);
+    const isAuthenticated = !!Meteor.userId();
+    const currentPagePrivacy = Session.get('currentPagePrivacy');
+    onAuthChange(isAuthenticated, currentPagePrivacy);
 });
 
 Tracker.autorun(() => {
-  const selectedNoteId = Session.get('selectedNoteId');
-  if(selectedNoteId) {
-    browserHistory.replace(`/dashboard/${selectedNoteId}`);
-  }
+    const selectedNoteId = Session.get('selectedNoteId');
+    if (selectedNoteId) {
+        Session.set('isNavOpen', false);
+        browserHistory.replace(`/dashboard/${selectedNoteId}`);
+    }
 });
-
+Tracker.autorun(() => {
+    const isNavOpen = Session.get('isNavOpen');
+    document.body.classList.toggle('is-nav-open', isNavOpen);
+});
 Meteor.startup(() => {
-  Session.set('selectedNoteId', undefined);
-  ReactDOM.render(routes, document.getElementById('app'));
+    Session.set('selectedNoteId', undefined);
+    Session.set('isNavOpen', false);
+    ReactDOM.render(routes, document.getElementById('app'));
 });
--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import { Accounts } from 'meteor/accounts-base';
-import { createContainer } from 'meteor/react-meteor-data';
-
+import {Accounts} from 'meteor/accounts-base';
+import {createContainer} from 'meteor/react-meteor-data';
+import {Session} from 'meteor/session';
 export const PrivateHeader = (props) => {
-  return (
-    <div className="header">
-      <div className="header__content">
-        <h1 className="header__title">{props.title}</h1>
-        <button className="button button--link-text" onClick={() => props.handleLogout()}>Logout</button>
-      </div>
-    </div>
-  );
+    const navImageSrc = props.isNavOpen ? '/images/x.svg' : '/images/bars.svg';
+    return (
+        <div className="header">
+            <div className="header__content">
+                <img className="header__nav-toggle" src={navImageSrc} onClick={props.handleNavToggle}/>
+                <h1 className="header__title">{props.title}</h1>
+                <button className="button button--link-text" onClick={() => props.handleLogout()}>Logout</button>
+            </div>
+        </div>
+    );
 };
 
 PrivateHeader.propTypes = {
-  title: React.PropTypes.string.isRequired,
-  handleLogout: React.PropTypes.func.isRequired
+    title: React.PropTypes.string.isRequired,
+    handleLogout: React.PropTypes.func.isRequired,
+    handleNavToggle: React.PropTypes.func.isRequired,
+    isNavOpen: React.PropTypes.bool.isRequired
 };
 
 export default createContainer(() => {
-  return {
-    handleLogout: () => Accounts.logout()
-  };
+    return {
+        handleLogout: () => Accounts.logout(),
+        handleNavToggle: () => Session.set('isNavOpen', !Session.get('isNavOpen')),
+        isNavOpen: Session.get('isNavOpen')
+    };
 }, PrivateHeader);
